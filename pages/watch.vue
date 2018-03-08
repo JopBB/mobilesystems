50c39b80d54e0814e47ecc15534f846d291d2dfc
Jop <git@jopbrandenburg.nl>
2018-03-08 14:48:32 +0100

    intialisation

---
+++ pages/watch.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Welcome!</h1>
+    <nuxt-link to="/">index page</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Watch page'
+  }
+}
+</script>
