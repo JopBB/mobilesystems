091b76aef9eb0a0aad4200d0bceb26d432cda5aa
Jop <jopbrandenburg@gmail.com, git@jopbrandenburg.nl>
2018-03-10 16:15:42 +0100

    add locations

--- pages/index.vue
+++ pages/index.vue
@@ -45,6 +45,15 @@
 		bottom:8%;
 	}
 
+	.locationPointer{
+		position: absolute;
+		top:50%;
+		left:50%;
+		width:10%;
+		height:10%;
+		margin: -10% 0 0 -5%;
+	}
+
 </style>
 
 
@@ -53,11 +62,29 @@
   	<div id="maps">
   		<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d4900.067924077857!2d5.124685750667434!3d52.115510841871725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2snl!4v1520606730254" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
   	</div>
+
+  	<div class="locationPointer">
+  		<img v-if="placeDangerLocation" src="DangerLocation.png">
+  	</div>
+  	
+  	<div class="locationPointer">
+  		<img v-if="placeCarLocation"  src="CarLocation.png">
+  	</div>
+  	
   	<div id="actionButtons">
-  		<div class="actionButton">
+  		<div v-on:click="switchDangerLocation()" class="actionButton">
   			<img src="dangerButton.png">
   		</div>
-  		<div class="actionButton">
+	  	
+	  	<div class="locationSetButton">
+  			<img v-if="placeCarLocation" v-on:click="confirmCarLocation()" src="setLocationCarButton.png">
+  		</div>
+
+	  	<div class="locationSetButton">
+	  		<img v-if="placeDangerLocation" v-on:click="confirmDangerLocation()" src="setLocationDangerButton.png">
+	  	</div>
+
+  		<div v-on:click="switchCarLocation()" class="actionButton">
   			<img src="carButton.png">
   		</div>
   	</div>
@@ -90,8 +117,14 @@ export default {
   head: {
     title: 'Home page'
   },
+  data() {
+  	return {
+  		placeDangerLocation: false,
+  		placeCarLocation: false
+  	}
+  },
   methods: {
-  	confirmDelete: function(){
+  	confirmDelete(){
   		swal({
 		  title: 'Are you sure you want to delete this project?',
 		  text: "You won't be able to revert this!",
@@ -109,7 +142,49 @@ export default {
 		    )
 		  }
 		})
-  	}
+  	},
+  	switchCarLocation(){
+  		this.placeCarLocation = !this.placeCarLocation;
+  		if(this.placeDangerLocation){
+  			this.placeDangerLocation = false;
+  		}
+  	},
+  	switchDangerLocation(){
+  		this.placeDangerLocation = !this.placeDangerLocation;
+  		if(this.placeCarLocation){
+  			this.placeCarLocation = false;
+  		}
+  	},
+  	confirmCarLocation(){
+  		swal({
+  			title: 'Are you sure you want to set the escape car to this location?',
+  			type: 'info',
+  			showCancelButton: true,
+  		}).then((result) => {
+		  if (result.value) {
+		    swal(
+		      'Done!',
+		      'The escape car location has been set!.',
+		      'success'
+		    )
+		  }
+		})
+  	},
+  	 confirmDangerLocation(){
+  		swal({
+  			title: 'Are you sure you want to set the danger-point car to this location?',
+  			type: 'info',
+  			showCancelButton: true,
+  		}).then((result) => {
+		  if (result.value) {
+		    swal(
+		      'Done!',
+		      'The danger point has been set, now get to the car-rendez-vous point as quick as possible!',
+		      'success'
+		    )
+		  }
+		})
+  	}	
   }
 }
 </script>
